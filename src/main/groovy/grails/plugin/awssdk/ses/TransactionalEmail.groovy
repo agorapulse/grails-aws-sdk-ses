c4d17e799180801b4a099a203020bc8a24591d33
Sergio del Amo <sergio.delamo@shoptimix.com>
2016-04-19 10:22:30 +0200

    Adds the possiblity to send an email with attachments

--- src/main/groovy/grails/plugin/awssdk/ses/TransactionalEmail.groovy
+++ src/main/groovy/grails/plugin/awssdk/ses/TransactionalEmail.groovy
@@ -4,23 +4,48 @@ import groovy.transform.CompileStatic
 
 @CompileStatic
 class TransactionalEmail {
-    String destinationEmail
+    String sourceEmail
     String subject
     String htmlBody = '<html><body></body></html>'
 
-    String sourceEmail
     String replyToEmail
 
+    List<String> recipients = []
+
+    List<TransactionalEmailAttachment> attachments = []
+
+    void attachment(Closure attachment) {
+
+        Closure cl = (Closure) attachment.clone()
+        TransactionalEmailAttachment att = new TransactionalEmailAttachment()
+        cl.delegate = att
+        cl.resolveStrategy = Closure.DELEGATE_FIRST
+        cl()
+        attachments << att
+
+    }
+
+    void recipients(List<String> recipients) {
+        this.recipients = recipients
+    }
+
     void to(String str) {
-        this.destinationEmail = str
+        this.recipients = [str]
     }
 
     void from(String str) {
         this.sourceEmail = str
     }
+    void sourceEmail(String str) {
+        this.sourceEmail = str
+    }
 
     void destinationEmail(String str) {
-        this.destinationEmail = str
+        this.recipients = [str]
+    }
+
+    String getDestinationEmail() {
+        return (!this.recipients.isEmpty()) ? this.recipients.first() : null
     }
 
     void subject(String str) {
@@ -31,9 +56,7 @@ class TransactionalEmail {
         this.htmlBody = str
     }
 
-    void sourceEmail(String str) {
-        this.sourceEmail = str
-    }
+
 
     void replyToEmail(String str) {
         this.replyToEmail = str
---
+++ src/main/groovy/grails/plugin/awssdk/ses/UnsupportedAttachmentTypeException.groovy
@@ -0,0 +1,7 @@
+package grails.plugin.awssdk.ses
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class UnsupportedAttachmentTypeException extends Exception {
+}
