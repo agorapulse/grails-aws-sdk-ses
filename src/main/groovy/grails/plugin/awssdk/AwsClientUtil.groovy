d586fc02c6d774b97860d460b6a3eb67a1503a65
sdelamo <sergio.delamo@softamo.com>
2016-11-14 11:46:33 +0100

    Use constants and static compilation

--- src/main/groovy/grails/plugin/awssdk/AwsClientUtil.groovy
+++ src/main/groovy/grails/plugin/awssdk/AwsClientUtil.groovy
@@ -3,10 +3,24 @@ package grails.plugin.awssdk
 import com.amazonaws.ClientConfiguration
 import com.amazonaws.Protocol
 import grails.config.Config
+import groovy.transform.CompileStatic
 
+@CompileStatic
 class AwsClientUtil {
 
     static final String DEFAULT_REGION = 'us-east-1'
+    public static final String CONNECTION_TIMEOUT = 'connectionTimeout'
+    public static final String MAX_CONNECTIONS = 'maxConnections'
+    public static final String MAX_ERROR_RETRY = 'maxErrorRetry'
+    public static final String PROTOCOL = 'protocol'
+    public static final String SOCKET_TIMEOUT = 'socketTimeout'
+    public static final String USER_AGENT = 'userAgent'
+    public static final String PROXY_DOMAIN = 'proxyDomain'
+    public static final String PROXY_HOST = 'proxyHost'
+    public static final String PROXY_PASSWORD = 'proxyPassword'
+    public static final String PROXY_PORT = 'proxyPort'
+    public static final String PROXY_USERNAME = 'proxyUsername'
+    public static final String PROXY_WORKSTATION = 'proxyWorkstation'
 
     static intValueForConfigName(Config co, String configName, final String serviceConfig,  final String defaultConfig, int defaultValue = 0) {
         def propertykey = "${serviceConfig}.${configName}" as String
@@ -33,18 +47,18 @@ class AwsClientUtil {
     }
 
     static ClientConfiguration clientConfigurationWithConfig(Config co, final String defaultConfig, final String serviceConfig) {
-        int connectionTimeout = intValueForConfigName(co, 'connectionTimeout', serviceConfig, defaultConfig)
-        int maxConnections = intValueForConfigName(co, 'maxConnections', serviceConfig,  defaultConfig)
-        int maxErrorRetry = intValueForConfigName(co, 'maxErrorRetry', serviceConfig, defaultConfig)
-        String protocol = stringValueForConfig(co, 'protocol', serviceConfig, defaultConfig)
-        int socketTimeout = intValueForConfigName(co, 'socketTimeout', serviceConfig, defaultConfig)
-        String userAgent = stringValueForConfig(co, 'userAgent', serviceConfig, defaultConfig)
-        String proxyDomain = stringValueForConfig(co, 'proxyDomain', serviceConfig, defaultConfig)
-        String proxyHost = stringValueForConfig(co, 'proxyHost', serviceConfig, defaultConfig)
-        String proxyPassword = stringValueForConfig(co, 'proxyPassword', serviceConfig, defaultConfig)
-        int proxyPort = intValueForConfigName(co, 'proxyPort', serviceConfig, defaultConfig)
-        String proxyUsername = stringValueForConfig(co, 'proxyUsername', serviceConfig, defaultConfig)
-        String proxyWorkstation = stringValueForConfig(co, 'proxyWorkstation', serviceConfig, defaultConfig)
+        int connectionTimeout = intValueForConfigName(co, CONNECTION_TIMEOUT, serviceConfig, defaultConfig)
+        int maxConnections = intValueForConfigName(co, MAX_CONNECTIONS, serviceConfig,  defaultConfig)
+        int maxErrorRetry = intValueForConfigName(co, MAX_ERROR_RETRY, serviceConfig, defaultConfig)
+        String protocol = stringValueForConfig(co, PROTOCOL, serviceConfig, defaultConfig)
+        int socketTimeout = intValueForConfigName(co, SOCKET_TIMEOUT, serviceConfig, defaultConfig)
+        String userAgent = stringValueForConfig(co, USER_AGENT, serviceConfig, defaultConfig)
+        String proxyDomain = stringValueForConfig(co, PROXY_DOMAIN, serviceConfig, defaultConfig)
+        String proxyHost = stringValueForConfig(co, PROXY_HOST, serviceConfig, defaultConfig)
+        String proxyPassword = stringValueForConfig(co, PROXY_PASSWORD, serviceConfig, defaultConfig)
+        int proxyPort = intValueForConfigName(co, PROXY_PORT, serviceConfig, defaultConfig)
+        String proxyUsername = stringValueForConfig(co, PROXY_USERNAME, serviceConfig, defaultConfig)
+        String proxyWorkstation = stringValueForConfig(co, PROXY_WORKSTATION, serviceConfig, defaultConfig)
 
         ClientConfiguration clientConfiguration = new ClientConfiguration()
         if (connectionTimeout){
@@ -57,7 +71,7 @@ class AwsClientUtil {
             clientConfiguration.maxErrorRetry = maxErrorRetry
         }
         if (protocol) {
-            if ( protocol.toUpperCase() == 'HTTP' ) {
+            if ( protocol.toUpperCase() == 'HTTP') {
                 clientConfiguration.protocol = Protocol.HTTP
             } else {
                 clientConfiguration.protocol = Protocol.HTTPS
